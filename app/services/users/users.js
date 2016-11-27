80ddf00b9df155a7d1189ce28edeb84e7b7fbc9b
Shakelia <sjohnson@inginestudio.com>
2016-11-27 02:30:21 -0500

    finished first part of karma-jasmine tutorial

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -1,1 +1,48 @@
+(function() {
+    'use strict';
 
+    //creating the module and factory we referenced in the beforeEach block in our test file
+    angular.module('api.users', [])
+        .factory('Users', function() {
+            var Users = {};
+            // The array of users our factory will provide us
+            var userList = [{
+                id: '1',
+                name: 'Jane',
+                role: 'Designer',
+                location: 'New York',
+                twitter: 'gijane'
+            }, {
+                id: '2',
+                name: 'Bob',
+                role: 'Developer',
+                location: 'New York',
+                twitter: 'billybob'
+            }, {
+                id: '3',
+                name: 'Jim',
+                role: 'Developer',
+                location: 'Chicago',
+                twitter: 'jimbo'
+            }, {
+                id: '4',
+                name: 'Bill',
+                role: 'Designer',
+                location: 'LA',
+                twitter: 'dabill'
+            }];
+            //Users.method = function() {};
+            Users.all = function() {
+                //return the array of users
+                return userList;
+            };
+
+            Users.findById = function(id) {
+                return userList.find(function(user) {
+                    return user.id === id;
+                });
+            };
+
+            return Users;
+        });
+})();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,1 +1,80 @@
+describe('Users factory', function() {
+    var Users;
+    // The array of users our factory will provide us
+    var userList = [{
+        id: '1',
+        name: 'Jane',
+        role: 'Designer',
+        location: 'New York',
+        twitter: 'gijane'
+    }, {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob'
+    }, {
+        id: '3',
+        name: 'Jim',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'jimbo'
+    }, {
+        id: '4',
+        name: 'Bill',
+        role: 'Designer',
+        location: 'LA',
+        twitter: 'dabill'
+    }];
 
+    var singleUser = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob'
+    };
+    //before each test load our api.users module
+    beforeEach(angular.mock.module('api.users'));
+
+    //before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function(_Users_) {
+        Users = _Users_;
+    }));
+
+    //a simple test to verify the Users factory exists
+    it('should exist', function() {
+        expect(Users).toBeDefined();
+    });
+
+    //a set of tests for our Users.all() method
+    describe('.all()', function() {
+        //a simple test to verify the method all exists
+        it('should exist', function() {
+            expect(Users.all).toBeDefined();
+        });
+
+        //a test to verify that calling all() returns the array of users we hard-coded above
+        it('should return a hard-coded list of users', function() {
+            expect(Users.all()).toEqual(userList);
+        });
+    });
+
+    //a set of tests for our Users.findById() method
+    describe('.findById()', function() {
+        //a simple test to verify the method findById exists
+        it('should exist', function() {
+            expect(Users.findById).toBeDefined();
+        });
+
+        //a test to verify that calling findById() with an id returns a single user
+        it('should return one user object if it exists', function() {
+            expect(Users.findById('2')).toEqual(singleUser);
+        });
+
+        //a test to verify that calling findById() with an id that doesn't exist returns undefined
+        it('should return undefined if the user cannot be found', function() {
+          expect(Users.findById('ABC')).not.toBeDefined();
+        });
+    });
+});
